516254290380a4ae5e741927fc14a378c23b1563
Luke Hagar <lukeslakemail@gmail.com>
2023-04-10 09:34:19 -0500

    removed paginator

--- index.ts
+++ index.ts
@@ -2,6 +2,4 @@ export * from "./sdk-output/api";
 
 export {Configuration, ConfigurationParameters} from "./sdk-output/configuration";
 
-export * from "./paginator";
-
 export * as axiosRetry from "axios-retry"
