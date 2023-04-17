bebf4d93ae290f8ce3728ab88855750e3d28b332
Luke Hagar <lukeslakemail@gmail.com>
2023-04-17 15:11:29 -0500

    cleanup

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./sdk-output/api";
-export { Configuration, ConfigurationParameters } from "./sdk-output/configuration";
-export * as axiosRetry from "axios-retry";
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./sdk-output/api";
-export { Configuration } from "./sdk-output/configuration";
-export * as axiosRetry from "axios-retry";
-//# sourceMappingURL=index.js.map
